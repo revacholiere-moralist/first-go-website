0c3ccfd16415b2618e44a7e8a6d89fb52af599e8
habibmalik <habibmalik.professional@gmail.com>
2025-01-15 20:56:28 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello, world!")
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
+}
